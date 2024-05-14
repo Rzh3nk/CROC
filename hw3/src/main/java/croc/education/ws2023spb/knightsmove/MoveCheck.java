6c4a1a00029c824412c52a9ef3d88e9da96bbcb5
Rzh3nk <147371569+Rzh3nk@users.noreply.github.com>
2024-05-14 15:07:07 +0800

    Update MoveCheck.java

--- hw3/src/main/java/croc/education/ws2023spb/knightsmove/MoveCheck.java
+++ hw3/src/main/java/croc/education/ws2023spb/knightsmove/MoveCheck.java
@@ -17,7 +17,8 @@ public class MoveCheck implements KnightsMoveChecker{
             ChessPosition pos = ChessPositionParser.parse(positions[0]); //задаеи начальную позицию
             for (int i = 1; i < positions.length; i++) {
                 ChessPosition newpos = ChessPositionParser.parse(positions[i]);//задаем позицию, куда хотим сходить
-                if ((Math.abs(newpos.x() - pos.x()) == 2 && Math.abs(newpos.y() - pos.y()) == 1) || (Math.abs(newpos.x() - pos.x()) == 1 && Math.abs(newpos.y() - pos.y()) == 2)) {
+                if ((Math.abs(newpos.x() - pos.x()) == 2 && Math.abs(newpos.y() - pos.y()) == 1) ||
+                        (Math.abs(newpos.x() - pos.x()) == 1 && Math.abs(newpos.y() - pos.y()) == 2)) {
                     pos = newpos; //если ход возможен, меняем позицию
                 } else {
                     String message;
